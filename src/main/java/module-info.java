4ba422c4c8aa99d271b2102fdb47555409db55fe
smedziko <98097684+smedziko@users.noreply.github.com>
2022-09-21 08:36:43 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module htl.steyr.ac.at.clashroyale {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens htl.steyr.ac.at.clashroyale to javafx.fxml;
+    exports htl.steyr.ac.at.clashroyale;
+}
