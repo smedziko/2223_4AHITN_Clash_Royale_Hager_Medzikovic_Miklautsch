113f11ac9c1093d20da85b9278e58a1f89ad7e97
emiklaut <95867791+emiklaut@users.noreply.github.com>
2022-10-05 10:42:39 +0200

    update

--- src/main/java/clash/royale/model/startMusic.java
+++ src/main/java/clash/royale/model/startMusic.java
@@ -1,7 +1,7 @@
 package clash.royale.model;
 
 public class startMusic {
-    musicThread thread = new musicThread("src/Tetris_1.mp3");
+    musicThread thread = new musicThread("src/indila-tourner-dans-le-vide-alphagospelmusiccom_N1IdhcRU.mp3");
     Thread thread2 = new Thread(thread);
     public void start() {
             thread2.start();
